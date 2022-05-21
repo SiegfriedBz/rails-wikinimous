67303830927e3520faafd6576b9d775bd658e1aa
siegfried bozza <siegfried.bozza@protonmail.com>
2022-05-21 19:22:43 +0200

    added form partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,22 +19,20 @@ class ArticlesController < ApplicationController
     else
       render :new
     end
-
-    def edit
-    end
-
-    def update
-      @article.update(article_params)
-      redirect_to article_path(@article)
-    end
-
-    def destroy
-      @article.destroy
-      redirect_to articles_path
-    end
-
   end
 
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
 
   private
 
